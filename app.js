b84c78c1b595d537f7a6fa184b6ce330f8be679b
Prasanth-R-01 <prasanthramaswamy01@gmail.com>
2026-01-01 07:50:49 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Kubernetes deployment successful 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
